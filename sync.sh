2c5a276fecc5dd0fbe73a91b74e8bb00dae81d99
Geet Patel <geetpofficial@gmail.com>
2025-08-23 21:04:58 -0400

    dunst + normal checkins

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 CONFIG="$HOME/.config/"
 
-dirs=("sway" "swaylock" "nvim" "rofi" "waybar" "ranger" "kitty")
+dirs=("sway" "swaylock" "nvim" "rofi" "waybar" "ranger" "kitty" "dunst")
 
 for dir in "${dirs[@]}"; do
     echo "$dir"
