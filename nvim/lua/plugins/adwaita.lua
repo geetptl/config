67f17754b419806b1e4df1650edf03484cc1a54a
geet <geetpofficial@gmail.com>
2026-05-14 19:08:46 -0400

    Nvim maintenance

--- nvim/lua/plugins/adwaita.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "Mofiqul/adwaita.nvim",
-    lazy = false,
-    priority = 1000,
-    -- configure and set on startup
-    config = function()
-        vim.cmd('colorscheme adwaita')
-    end
-}
--- nvim/lua/plugins/autoclose.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "m4xshen/autoclose.nvim",
-    config = function()
-        local autoclose = require("autoclose")
-
-        autoclose.setup({})
-    end
-}
